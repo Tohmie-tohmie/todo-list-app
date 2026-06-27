738fb6da92ec901ab27a3c1bac5970d44c71d5f6
Ajayi oluwatomisin Testimony <ajayitomisin99@gmail.com>
2026-06-27 07:45:40 +0200

    Create TodoForm component and basic layout

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import { TodoForm } from './components/TodoForm.jsx';
+
+function App() {
+
+  return(
+  <>
+      <TodoForm />
+  </>
+   
+  );
+}
+
+export default App;
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,16 @@
+
+
+
+export function TodoForm() {
+
+  return(
+   
+    <div>
+      <h1>
+        MY TO-DO-LIST
+      </h1>
+        <input type="text" placeholder="Enter a task..." />
+        <button>Add</button>
+    </div>
+  );
+}
