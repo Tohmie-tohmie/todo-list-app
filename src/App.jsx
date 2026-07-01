fde32ca3d1a6f415ea9bb54c53ca69a0d4749f3c
Ajayi oluwatomisin Testimony <ajayitomisin99@gmail.com>
2026-07-01 02:24:41 +0200

    Add control input and form submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
 
+import './App.css';
 import { TodoForm } from './components/TodoForm.jsx';
 
 function App() {
 
   return(
-  <>
+  <div className="app">
+     <h1>
+        MY TO-DO-LIST
+      </h1>
       <TodoForm />
-  </>
+  </div>
    
   );
 }
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,16 +1,41 @@
 
+import { useState } from 'react';
 
 
 export function TodoForm() {
+   
+
+  const [task, setTask] = useState("");
+
+    function handleChange(event) {
+      setTask(event.target.value);
+    }
+
+    function handleSubnmit() {
+      if(task === "") {
+          return;
+      }
+
+       console.log(task);
+        setTask("");
+    }
+  
 
   return(
    
-    <div>
-      <h1>
-        MY TO-DO-LIST
-      </h1>
-        <input type="text" placeholder="Enter a task..." />
-        <button>Add</button>
+    <div className="todo-form">
+        <input
+        className="todo-input"
+         type="text" 
+         placeholder="Enter a task..."
+         value={task}
+         onChange={handleChange}
+         />
+        <button className="add-btn"
+        onClick={handleSubnmit}>
+          Add Task
+        </button>
+        <p>{task}</p>
     </div>
   );
 }
