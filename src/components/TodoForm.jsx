96377a37e1f01ee2f36faa02c926931d29a766d4
Ajayi oluwatomisin Testimony <ajayitomisin99@gmail.com>
2026-07-04 05:10:51 +0200

    Display tasks in a list

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -6,36 +6,42 @@ export function TodoForm() {
    
 
   const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
 
     function handleChange(event) {
       setTask(event.target.value);
     }
 
-    function handleSubnmit() {
-      if(task === "") {
+    function handleSubmit() {
+      if(task.trim() === "") {
           return;
       }
 
-       console.log(task);
+       setTasks([...tasks, task]);
         setTask("");
     }
   
 
   return(
-   
-    <div className="todo-form">
-        <input
-        className="todo-input"
-         type="text" 
-         placeholder="Enter a task..."
-         value={task}
-         onChange={handleChange}
-         />
-        <button className="add-btn"
-        onClick={handleSubnmit}>
-          Add Task
-        </button>
-        <p>{task}</p>
-    </div>
+   <>
+        <div className="todo-form">
+            <input
+            className="todo-input"
+            type="text" 
+            placeholder="Enter a task..."
+            value={task}
+            onChange={handleChange}
+            />
+            <button className="add-btn"
+            onClick={handleSubmit}>
+              Add Task
+            </button>
+      </div>
+            <div className="todo-list">
+            {tasks.map((task, index) => (
+                <p key={index}>{task}</p>
+            ))}
+            </div>
+    </>      
   );
 }
