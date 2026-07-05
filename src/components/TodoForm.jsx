010e5511f268bcc92a8f5c75c4f84c47fcee9dcd
Ajayi oluwatomisin Testimony <ajayitomisin99@gmail.com>
2026-07-05 09:51:03 +0200

    Complet delete task feature

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -20,6 +20,13 @@ export function TodoForm() {
        setTasks([...tasks, task]);
         setTask("");
     }
+
+    function deleteTask(indexToDelete) {
+      const updateTasks = tasks.filter((task, index) => {
+        return index !== indexToDelete;
+      });
+       setTasks(updateTasks); 
+    }
   
 
   return(
@@ -39,7 +46,14 @@ export function TodoForm() {
       </div>
             <div className="todo-list">
             {tasks.map((task, index) => (
-                <p key={index}>{task}</p>
+                <div key={index}
+                   className="task-item">
+                  <p>{task}</p>
+                  <button className="delete-btn"
+                  onClick={() => deleteTask(index)}>
+                    Delete
+                   </button>
+                  </div>
             ))}
             </div>
     </>      
